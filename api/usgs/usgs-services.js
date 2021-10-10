db157b6fec04db883e01a956ec9b38b2160a2b2d
raymondeyan <yanr3103@lgsstudent.org>
2021-10-10 17:22:30 -0400

    huc usgs endpoint implemented

--- api/usgs/usgs-services.js
+++ api/usgs/usgs-services.js
@@ -1,3 +1,70 @@
 /**
  * Helper functions may be used to process and clean up the data
+ * refer to usgs-query-example.json for structure of an USGS response json
  */
+module.exports = {
+  /**
+   * @param {data, extract} object and array of int IDs of the values to extract, array can be of any length
+   * @returns {object} object of desired values
+   * For v1, only gage height and water temp will be returned
+   * Not every site has measurements on gage height and water temp, the sites that have only one measurement type will return null for the other measurement type
+   */
+  extractWaterValues: async (data) => {
+    try {
+
+      let processedData = {};
+    
+    for (let i = 0; i < data.value.timeSeries.length; i++) {
+      //if the site is a new site, push into array of vals, and set the other measurement value as null
+      //if the site shows up again, the other measurement type will be set to the correct type
+      //if the site doesn't measure that type, it will stay null
+
+      if (!(processedData.hasOwnProperty(
+        data.value.timeSeries[i].sourceInfo.siteName)))
+        {
+          if (data.value.timeSeries[i].variable.variableCode[0].value==00065){
+            //take the most recent value (for now)
+            //because there are varying amounts of objects in the values array for each location
+            gageHeight = data.value.timeSeries[i].values[0].value[0].value;
+            gageHeightTime = data.value.timeSeries[i].values[0].value[0].dateTime;
+            waterTemp = null;
+            waterTempTime = null;
+          }
+          else {
+            //since we only query for gageHeight and waterTemp(for now)
+            waterTemp = data.value.timeSeries[i].values[0].value[0].value;
+            waterTempTime = data.value.timeSeries[i].values[0].value[0].dateTime;
+            gageHeight = null;
+            gageHeightTime = null;
+          }
+          processedData[data.value.timeSeries[i].sourceInfo.siteName]={gageHeight, gageHeightTime, waterTemp, waterTempTime};
+        }
+      else 
+        {
+          //else we already have values for this site
+          //check which values we already have
+          //if gageHeight is null, that means there are existing values for waterTemp and vice-versa
+          if (processedData[data.value.timeSeries[i].sourceInfo.siteName].gageHeight===null){
+            gageHeight = data.value.timeSeries[i].values[0].value[0].value;
+            gageHeightTime = data.value.timeSeries[i].values[0].value[0].dateTime;
+            waterTemp = processedData[data.value.timeSeries[i].sourceInfo.siteName].waterTemp;
+            waterTempTime = processedData[data.value.timeSeries[i].sourceInfo.siteName].waterTempTime;
+          }
+          else {
+            waterTemp = data.value.timeSeries[i].values[0].value[0].value;
+            waterTempTime = data.value.timeSeries[i].values[0].value[0].dateTime;
+            gageHeight = processedData[data.value.timeSeries[i].sourceInfo.siteName].gageHeight;
+            gageHeightTime = processedData[data.value.timeSeries[i].sourceInfo.siteName].gageHeightTime;
+          }
+          processedData[data.value.timeSeries[i].sourceInfo.siteName]={gageHeight, gageHeightTime, waterTemp, waterTempTime};
+        }
+    }
+    //console.log(processedData)
+    return processedData;
+
+    } catch (error) {
+      console.log("error occured inside data processing service:", error);
+      return {error};
+    }
+  }
+}
--- api/usgs/usgs.controller.js
+++ api/usgs/usgs.controller.js
@@ -1,10 +1,13 @@
+const axios = require('axios');
+const service = require('./usgs-services.js');
+
 module.exports = {
   /**
-   * @param {int} site - the specific USGS measurement site
+   * @param {req, res} site param - the specific USGS measurement site
    * @returns {object} an object of values {}
    * will return more than huc since site is data in only one location
    */
-  site: async (site) => {
+  site: async (req,res) => {
     try {
       res.json({"site": "random site"})
     } catch (error) {
@@ -13,11 +16,11 @@ module.exports = {
   },
 
   /**
-   * @param {int} county - US county code
+   * @param {req, res} county param - US county code
    * @returns {object} an object of values {}
    * will return more than huc since county is data in only one location
    */
-  county: async (county) => {
+  county: async (req, res) => {
     try {
       res.json({"site": "random county"})
     } catch (error) {
@@ -26,14 +29,35 @@ module.exports = {
   },
 
   /**
-   * @param {int} huc - USGS' code assigned to a certain region
-   * @returns {array} an array of objects {LocationName, GageHeight, WaterTemp}
-   * basic version of api will return only LocationName, GageHieght, and Water Temp
+   * @param {req, res} huc param - USGS' code assigned to a certain region
+   * @returns {object} an object of objects {LocationName, GageHeight, WaterTemp}
+   * v1 will return only LocationName, GageHieght, and Water Temp
    */
-  huc: async (huc) => {
+  huc: async (req, res) => {
     try {
-      res.json({"site": "huc"})
+      huc=String(req.query.huc)
+      //check if huc id is valid
+
+      /**
+       * For complete parameters:
+       * https://help.waterdata.usgs.gov/codes-and-parameters/parameters
+       * Note: ID's and HUC must be strings or else JS will truncate the leading 0's, which are necessary for the url params
+       */
+      const gageHeightId = "00065";
+      const waterTempId = "00010";
+
+      let result = await axios.get(`https://waterservices.usgs.gov/nwis/iv/?format=json&huc=${huc}&parameterCd=${gageHeightId},${waterTempId}&siteStatus=all`);
+      
+      if (result.data.hasOwnProperty("name")){
+        console.log(`Successful GET request made from USGS at ${new Date().toString()}`);
+      }
+      else {
+        res.json(500).json({error: "Successful req, but incorrect data received"})
+      }
+      //console.log(await service.extractWaterValues(result.data))
+      res.status(200).json(await service.extractWaterValues(result.data));
     } catch (error) {
+      console.log(`GET request failed: ${error}`)
       res.status(500).json({error});
     }
   }
