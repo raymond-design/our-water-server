5b364c24cff845ddc22957dae4518da952d5addd
raymondeyan <raymondyan777@gmail.com>
2022-01-08 21:29:00 -0500

    viewbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Slider from './Components/Slider/Slider'
+import FadeIn from 'react-fade-in'
 
 function App() {
   return (
-    <>
-      <Slider />
-    </>
+    <div class="full">
+      <FadeIn transitionDuration="600" delay="250">
+        <h1 class="title">Water Map Viewer</h1>
+        <h2 class="title2">Real-Time Data in the U.S.</h2>
+        <Slider/>
+      </FadeIn>
+    </div>
   );
 }
 
